839c0e7bd3e61205f713e81dc07bfcfb4b76a808
İsmail Kattan <103933327+IsmailKattan@users.noreply.github.com>
2024-12-11 03:14:42 +0300

    model classes created

---
+++ services/address-service/src/main/java/com/gorevce/address_service/model/Address.java
@@ -0,0 +1,21 @@
+package com.gorevce.address_service.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "addresses")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Address {
+    @Id
+    private String id;
+    private String country;
+    private String city;
+    private String street;
+    private String postalCode;
+    private String additionalInfo;
+}
